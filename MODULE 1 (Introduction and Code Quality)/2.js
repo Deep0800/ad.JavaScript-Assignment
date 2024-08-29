0c40ba152d93b93943386258065acd738a2d2c9e
Deep0800 <97604349+Deep0800@users.noreply.github.com>
2024-08-29 22:22:02 +0530

    Add files via upload

---
+++ MODULE 1 (Introduction and Code Quality)/2.js
@@ -0,0 +1,22 @@
+// What will be the result for these expressions?
+
+// 1. 5 > 4
+console.log(5 > 4) //o/p: true
+
+// 2. "apple" > "pineapple"
+console.log("apple" > "pineapple") //o/p: false
+
+// 3. "2" > "12"
+console.log("2" > "12"); //o/p: true
+
+// 4. undefined == null
+console.log(undefined == null) //o/p: true
+
+// 5. undefined === null
+console.log(undefined === null) //o/p: false
+
+// 6. null == "\n0\n"
+console.log(null == "\n0\n") //o/p: false
+
+// 7. null === +"\n0\n"
+console.log(null === +"\n0\n") //o/p: false
